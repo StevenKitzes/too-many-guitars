c8c67e11cb8ed2778b484c5f70bd3db8f81ca4c5
Steven Kitzes <steven.kitzes@gmail.com>
2023-07-24 14:45:16 -0700

    fix: touch up gh-pages build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,27 +1,37 @@
+echo ""
 echo "Build and push to gh-pages"
 
+echo ""
 echo "Build step:"
 echo "$ npm run build"
 npm run build
 
+echo ""
 echo "Check out GitHub Pages branch:"
 echo "$ git checkout gh-pages"
 git checkout gh-pages
 
+echo ""
 echo "Copy build folder to root:"
 echo "$ cp -R build/ ."
 cp -R build/ .
 
+echo ""
 echo "Create Git commit:"
 echo "$ git commit -am 'updating gh-pages content . . .'"
 git commit -am "updating gh-pages content . . ."
 
+echo ""
 echo "Push:"
 echo "$ git push -u origin gh-pages"
 git push -u origin gh-pages
 
+echo ""
 echo "Return to main branch:"
 echo "$ git checkout main"
 git checkout main
 
+echo ""
 echo "All done!"
+
+echo ""
