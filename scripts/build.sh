b9f19292cefabc1db6a627254f73d9d6ac37f9f8
Steven Kitzes <steven.kitzes@gmail.com>
2023-07-24 14:41:08 -0700

    feat: flesh out gh-pages submission script

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,3 +10,18 @@ git checkout gh-pages
 
 echo "Copy build folder to root:"
 echo "$ cp -R build/ ."
+cp -R build/ .
+
+echo "Create Git commit:"
+echo "$ git commit -am 'updating gh-pages content . . .'"
+git commit -am "updating gh-pages content . . ."
+
+echo "Push:"
+echo "$ git push -u origin gh-pages"
+git push -u origin gh-pages
+
+echo "Return to main branch:"
+echo "$ git checkout main"
+git checkout main
+
+echo "All done!"
